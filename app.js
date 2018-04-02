046fdb315d02b3e4ceb9940c6ecbcd07aa3516a6
harshit kohli <harshit.kohli1997@gmail.com>
2018-04-03 02:40:55 +0530

    edit app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
 const path = require('path');
+const http = require('http');
 const app = express();
+ var server = http.createServer(app);
+//setting up socket io
+const socketIO = require('socket.io'); 
+var io = socketIO(server);
 
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
@@ -17,8 +22,15 @@ app.engine('handlebars', exphbs({
 app.use(express.static(path.join(__dirname, 'public')));
 
 //setting server
-
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
+
+io.on('connection', (socket) => {
+console.log('new user connected');
+
+socket.on('disconnect', () => {
+  console.log('user was diconnected');
+})
+})
+server.listen(port, () => {
     console.log(`server started on port ${port}`)
 })
