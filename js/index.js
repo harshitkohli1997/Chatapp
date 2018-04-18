4ca96c772fb5ef347cf0c6de4d305c34f48518bb
harshit kohli <harshit.kohli1997@gmail.com>
2018-04-19 01:59:18 +0530

    working prototype of chatapp

--- js/index.js
+++ js/index.js
@@ -14,12 +14,21 @@ socket.on('disconnect', () => {
 
 socket.on('newMessage', (message) => {
     console.log('message is',message);
+    //using jquery to create modify and make element visible
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(li);
 })
 
+
+//selecting the from and overwriting its it behaviour
+jQuery('#message-form').on('submit',  (e) =>{
+  e.preventDefault();
 socket.emit('createMessage', {
-  from:'User',
-  text:req.body.message
-},
-function()  {
-  console.log('message sent');
+  from:'scooby',
+text: jQuery('[name=message]').val()
+
+}, () => {
+console.log('sent');
+}) ;
 });
