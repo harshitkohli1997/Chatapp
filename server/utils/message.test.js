bbb4ab3fd2992132f963c32a931cd8ed0326ab59
harshit kohli <harshit.kohli1997@gmail.com>
2018-04-21 23:24:16 +0530

    second test pass

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var { generateMessage } = require('./message');
+var { generateMessage ,generateLocationMessage } = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object',() => {
@@ -15,4 +15,17 @@ describe('generateMessage', () => {
     //assert text match
     //assert createAT is number
     });
+});
+    describe('generateLocationMessage', () => {
+        it('should generate correct location object',() => {
+       var from  = 'hk';
+       var latitude = '15';
+       var longitude ='19';
+       var url = 'http://www.google.com/maps?q=15,19'
+        var message = generateLocationMessage(from,latitude,longitude);
+
+       expect(typeof message.createAt).toBe('number');
+       expect(message).toMatchObject({from,url});
+        
+        });
 })
