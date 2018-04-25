3309cb6e474cf782f64a0d604a48b4f1a9af329d
harshit kohli <harshit.kohli1997@gmail.com>
2018-04-26 01:41:14 +0530

    timestamp done

--- js/index.js
+++ js/index.js
@@ -18,10 +18,11 @@ socket.on('newMessage', function (message) {
 });
 
 socket.on('newLocationMessage', function (message) {
+  var formattedTime = moment(message.createAt).format('h:mm a')
   var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">My current location</a>');
 
-  li.text(`${message.from}: `);
+  li.text(`${message.from} ${formattedTime}: `);
   a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
